15101be0c63bbedd8e3362945a7fa93a66e01e7e
Niko94usa <86586147+Niko94usa@users.noreply.github.com>
2023-08-11 17:37:47 +0300

    Update list-node.js

--- list-node.js
+++ list-node.js
@@ -9,7 +9,17 @@ module.exports = {
   ListNode
 };
 class ListNode {
+  constructor(x) {class ListNode {
   constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
+
+module.exports = {
+  ListNode
+};
+
     this.value = x;
     this.next = null;
   }
