940f9bf93d6464668527fd026b51f4ba484f5b8f
Niko94usa <86586147+Niko94usa@users.noreply.github.com>
2023-10-25 11:34:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-ASDFASDFSASDASDASconst { checkForThrowingErrors, checkForNotThrowingErrors } = require('./check-error.js');
+ASDFASASDFASDFSASDASDASconst { checkForThrowingErrors, checkForNotThrowingErrors } = require('./check-error.js');
 const { CONSTANTS } = require('./constants.js');
 const { testOptional } = require('./it-optional.js');
 const { NotImplementedError } = require('./not-implemented-error.js');
